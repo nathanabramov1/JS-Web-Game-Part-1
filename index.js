f832a3932bd7f85d399504574256a7cf6ab32815
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-05-14 11:52:57 -0400

    not finished

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(src, positionLeft, positionBottom){
+    let pineTree = document.createElement('img')
+    pineTree.src = src
+    pineTree.style.position = 'fixed'
+    pineTree.style.left = positionLeft
+    pineTree.style.bottom = positionBottom
+    document.body.append(pineTree) 
+}
+function newItem(src, positionLeft, positionBottom){
+    let sword = document.createElement('img')
+    sword.src = src
+    sword.position = 'fixed'
+    sword.left = positionLeft
+    sword.bottom = positionBottom
+    document.body.append(sword)
+    sword.addEventListener('click',function(){
+        sword.remove()
+    })
+    
+}
+
+newImage('/assets/pine-tree.png','450px','200px')
+
+newImage('assets/green-character.gif', '100px', '100px')
+
+newImage('assets/tree.png','200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+newItem('assets/sword.png', '500px', '405px')
